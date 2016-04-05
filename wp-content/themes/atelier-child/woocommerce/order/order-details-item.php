46581585916f77b9142f199d66f45e284e1e9581
Tuffon <tbenavides@gravity.com>
2016-04-05 15:02:29 -0700

    restored

--- wp-content/themes/atelier-child/woocommerce/order/order-details-item.php
+++ wp-content/themes/atelier-child/woocommerce/order/order-details-item.php
@@ -34,7 +34,7 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 			do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order );
 
 			$order->display_item_meta( $item );
-			displayDownloads( $item );
+			$order->display_item_downloads( $item );
 
 			do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order );
 		?>
@@ -48,75 +48,3 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 	<td colspan="3"><?php echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) ); ?></td>
 </tr>
 <?php endif; ?>
-
-<?php
-
-function displayDownloads( $item ){
-	$product = getProductFromItem( $item );
-
-	if ( $product && $product->exists() && $product->is_downloadable() && $this->is_download_permitted() ) {
-		$download_files = getItemDownloads( $item );
-		$i              = 0;
-		$links          = array();
-
-		foreach ( $download_files as $download_id => $file ) {
-			$i++;
-			$prefix  = count( $download_files ) > 1 ? sprintf( __( 'Download %d', 'woocommerce' ), $i ) : __( 'Download', 'woocommerce' );
-			$links[] = '<span class="download-url">' . $prefix . ': <a href="' . esc_url( $file['download_url'] ) . '" target="_blank">' . esc_html( $file['name'] ) . '</a></span>' . "\n";
-		}
-
-		echo '<br/>' . implode( '<br/>', $links );
-	}
-}
-
-function getProductFromItem( $item ){
-	if ( ! empty( $item['variation_id'] ) && 'product_variation' === get_post_type( $item['variation_id'] ) ) {
-		$_product = wc_get_product( $item['variation_id'] );
-	} elseif ( ! empty( $item['product_id']  ) ) {
-		$_product = wc_get_product( $item['product_id'] );
-	} else {
-		$_product = false;
-	}
-
-	return apply_filters( 'woocommerce_get_product_from_item', $_product, $item, $this );
-}
-
-/**
- * Get the downloadable files for an item in this order.
- *
- * @param  array $item
- * @return array
- */
-function getItemDownloads( $item ) {
-	global $wpdb;
-
-	$product_id   = $item['variation_id'] > 0 ? $item['variation_id'] : $item['product_id'];
-	$product      = wc_get_product( $product_id );
-	if ( ! $product ) {
-		/**
-		 * $product can be `false`. Example: checking an old order, when a product or variation has been deleted.
-		 * @see \WC_Product_Factory::get_product
-		 */
-		return array();
-	}
-	$download_ids = $wpdb->get_col( $wpdb->prepare("
-			SELECT download_id
-			FROM {$wpdb->prefix}woocommerce_downloadable_product_permissions
-			WHERE user_email = %s
-			AND order_key = %s
-			AND product_id = %s
-			ORDER BY permission_id
-		", $this->billing_email, $this->order_key, $product_id ) );
-
-	$files = array();
-
-	foreach ( $download_ids as $download_id ) {
-
-		if ( $product->has_file( $download_id ) ) {
-			$files[ $download_id ]                 = $product->get_file( $download_id );
-			$files[ $download_id ]['download_url'] = $this->get_download_url( $product_id, $download_id );
-		}
-	}
-
-	return apply_filters( 'woocommerce_get_item_downloads', $files, $item, $this );
-}
